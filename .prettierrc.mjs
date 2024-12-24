7f53c82ce7b2c16a248daa53388aec740ef0d8d2
jada-gwon <jada.gwon@gmail.com>
2024-12-24 09:41:42 +0900

    chore: eslint 설정 구성

---
+++ .prettierrc.mjs
@@ -0,0 +1,14 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 80,
+  useTabs: false,
+};
+
+export default config;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,51 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import tailwind from 'eslint-plugin-tailwindcss';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  prettierRecommended,
+  ...tailwind.configs['flat/recommended'],
+  ...compat.config({
+    rules: {
+      'import/order': [
+        'error',
+        {
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          distinctGroup: false,
+          groups: [
+            ['builtin', 'external'],
+            'internal',
+            ['parent', 'sibling', 'index'],
+            'unknown',
+          ],
+          pathGroups: [
+            {
+              pattern: '@/**',
+              group: 'internal',
+            },
+            {
+              pattern: '__mocks__/**',
+              group: 'unknown',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react', 'react/**'],
+        },
+      ],
+    },
+  }),
 ];
 
 export default eslintConfig;
