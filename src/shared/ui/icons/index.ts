a33e06e71f945e0b7ecd93459fa29ea276fa7293
jada-gwon <jada.gwon@gmail.com>
2024-12-27 14:41:32 +0900

    chore: 로딩 아이콘 추가

--- src/shared/ui/icons/index.ts
+++ src/shared/ui/icons/index.ts
@@ -1,6 +1,13 @@
+import IconBarFade from './bar-fade.svg';
 import IconCheck from './check.svg';
 import IconChevronDown from './chevron-down.svg';
 import IconChevronLeft from './chevron-left.svg';
 import IconChevronRight from './chevron-right.svg';
 
-export { IconCheck, IconChevronDown, IconChevronLeft, IconChevronRight };
+export {
+  IconBarFade,
+  IconCheck,
+  IconChevronDown,
+  IconChevronLeft,
+  IconChevronRight,
+};
